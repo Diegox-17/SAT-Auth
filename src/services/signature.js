718124743e33aca1111a0060be073056c3b429de
Diegox-17 <direccion@consiliumproyectos.com>
2025-10-20 16:01:54 -0600

    Refactor signature handling with CustomKeyInfoProvider

--- src/services/signature.js
+++ src/services/signature.js
@@ -3,8 +3,17 @@ const SignedXml = require('xml-crypto').SignedXml;
 const forge = require('node-forge');
 const { processCertificate, decryptPrivateKey } = require('../utils/crypto');
 
+// Clase dedicada para manejar el KeyInfo de forma robusta
+function CustomKeyInfoProvider(tokenId) {
+    this.getKeyInfo = function (key, prefix) {
+        prefix = prefix ? prefix + ':' : '';
+        // Esta es la estructura exacta que el SAT espera
+        return `<${prefix}SecurityTokenReference><${prefix}Reference URI="#${tokenId}" ValueType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3"/></${prefix}SecurityTokenReference>`;
+    };
+}
+
 function createAuthSignature(cerBase64, keyPem, password) {
-    console.log('[SIGNATURE] v6.0 - Ejecutando para xml-crypto@3.3.0');
+    console.log('[SIGNATURE] v8.0 FINAL - Ejecutando para xml-crypto@2.1.3');
 
     const { pureCertBase64 } = processCertificate(cerBase64);
     const privateKey = decryptPrivateKey(keyPem, password);
@@ -37,18 +46,19 @@ function createAuthSignature(cerBase64, keyPem, password) {
 
     console.log('[SIGNATURE] Firmando XML...');
     const sig = new SignedXml();
+    // Le enseñamos a la firma sobre los namespaces que usaremos
+    sig.addNamespace('o', 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd');
+    
     sig.signingKey = privateKeyPem;
     sig.signatureAlgorithm = "http://www.w3.org/2000/09/xmldsig#rsa-sha1";
-    sig.keyInfoProvider = {
-        getKeyInfo: () => `<o:SecurityTokenReference><o:Reference ValueType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3" URI="#${securityTokenId}"/></o:SecurityTokenReference>`
-    };
+    sig.keyInfoProvider = new CustomKeyInfoProvider(securityTokenId);
+    
+    // Usamos la referencia URI, la forma más estándar de apuntar a un ID
+    sig.addReference(`//*[@u:Id="${timestampId}"]`, ["http://www.w3.org/2001/10/xml-exc-c14n#"], "http://www.w3.org/2000/09/xmldsig#sha1");
 
-    const xpath = "//*[local-name(.)='Timestamp']";
-    const transforms = ["http://www.w3.org/2001/10/xml-exc-c14n#"];
-    const digestAlgorithm = "http://www.w3.org/2000/09/xmldsig#sha1";
-
-    sig.addReference(xpath, transforms, digestAlgorithm, `#${timestampId}`);
-    sig.computeSignature(xml, { prefix: 'o', location: { reference: "//*[local-name()='Security']", action: 'append' } });
+    sig.computeSignature(xml, { 
+        location: { reference: "//*[local-name(.)='Security']", action: 'append' } 
+    });
     
     console.log('[SIGNATURE] Firma completada.');
     return sig.getSignedXml();
