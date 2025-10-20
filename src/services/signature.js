3cc1ca7e8f956b36e7e2bba83a347d8c3c759d09
Diegox-17 <direccion@consiliumproyectos.com>
2025-10-20 16:04:49 -0600

    Refactor signature.js to improve KeyInfo handling

--- src/services/signature.js
+++ src/services/signature.js
@@ -3,17 +3,8 @@ const SignedXml = require('xml-crypto').SignedXml;
 const forge = require('node-forge');
 const { processCertificate, decryptPrivateKey } = require('../utils/crypto');
 
-// Clase dedicada para manejar el KeyInfo de forma robusta
-function CustomKeyInfoProvider(tokenId) {
-    this.getKeyInfo = function (key, prefix) {
-        prefix = prefix ? prefix + ':' : '';
-        // Esta es la estructura exacta que el SAT espera
-        return `<${prefix}SecurityTokenReference><${prefix}Reference URI="#${tokenId}" ValueType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3"/></${prefix}SecurityTokenReference>`;
-    };
-}
-
 function createAuthSignature(cerBase64, keyPem, password) {
-    console.log('[SIGNATURE] v8.0 FINAL - Ejecutando para xml-crypto@2.1.3');
+    console.log('[SIGNATURE] v10.0 - Sintaxis final para xml-crypto@2.1.3');
 
     const { pureCertBase64 } = processCertificate(cerBase64);
     const privateKey = decryptPrivateKey(keyPem, password);
@@ -46,18 +37,24 @@ function createAuthSignature(cerBase64, keyPem, password) {
 
     console.log('[SIGNATURE] Firmando XML...');
     const sig = new SignedXml();
-    // Le enseñamos a la firma sobre los namespaces que usaremos
-    sig.addNamespace('o', 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd');
-    
     sig.signingKey = privateKeyPem;
     sig.signatureAlgorithm = "http://www.w3.org/2000/09/xmldsig#rsa-sha1";
-    sig.keyInfoProvider = new CustomKeyInfoProvider(securityTokenId);
     
-    // Usamos la referencia URI, la forma más estándar de apuntar a un ID
-    sig.addReference(`//*[@u:Id="${timestampId}"]`, ["http://www.w3.org/2001/10/xml-exc-c14n#"], "http://www.w3.org/2000/09/xmldsig#sha1");
+    // El KeyInfoProvider debe definir su propio namespace si no se define globalmente
+    sig.keyInfoProvider = {
+        getKeyInfo: () => `<o:SecurityTokenReference xmlns:o="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"><o:Reference ValueType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3" URI="#${securityTokenId}"/></o:SecurityTokenReference>`
+    };
+    
+    // Usamos el XPath robusto que es nuestra mejor opción para evitar el error InvalidSecurity
+    const xpath = "//*[local-name(.)='Timestamp']";
+    const transforms = ["http://www.w3.org/2001/10/xml-exc-c14n#"];
+    const digestAlgorithm = "http://www.w3.org/2000/09/xmldsig#sha1";
+
+    sig.addReference(xpath, transforms, digestAlgorithm);
 
     sig.computeSignature(xml, { 
-        location: { reference: "//*[local-name(.)='Security']", action: 'append' } 
+        prefix: 'o', 
+        location: { reference: "//*[local-name()='Security']", action: 'append' } 
     });
     
     console.log('[SIGNATURE] Firma completada.');
