23048869c7b4929beb1f40f543e39b0c520aac9b
Diegox-17 <direccion@consiliumproyectos.com>
2025-10-21 19:06:27 -0600

    Se agrega Paquetes para descargas

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRouter = require('./routes/auth');
 const descargaRouter = require('./routes/descarga'); // Descarga
 const verificacionRoutes = require('./routes/verificacion');
+const paquetesRoutes = require('./routes/paquetes');
 
 const app = express();
 const port = 3000;
@@ -14,6 +15,7 @@ app.use(express.static('public')); // Para servir nuestra API-LAB
 app.use('/auth', authRouter);
 app.use('/descarga', descargaRouter); // Descarga
 app.use('/descarga/verificar', verificacionRoutes);
+app.use('/descarga/paquetes', paquetesRoutes);
 app.listen(port, () => {
     console.log(`Servicio SAT escuchando en http://localhost:${port}`);
 });
